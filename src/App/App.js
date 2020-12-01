45e35f909a8e834ce2f396d8709aef85ef75568b
AL-Laith Dababseh <45473839+aldabbasy@users.noreply.github.com>
2020-12-01 23:31:07 +0200

    Added Search Functionality

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route } from 'react-router-dom'
 import MyReads from '../Screens/MyReads'
 import SearchBooks from '../Screens/SearchBooks'
 import './App.css'
 export default function App() {
+  
   return (
     <div className="app">
       <Route exact path='/' render={() => (
           <MyReads/>
         )} />
-      <Route exact path='/Search' render={() => (
-        <SearchBooks/>
+      <Route path='/Search' render={() => (
+        <SearchBooks />
         )} />
     </div>
   )
--- src/Screens/SearchBooks.js
+++ src/Screens/SearchBooks.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Book from '../Components/Book'
+import * as CallingAPI from '../Services/BooksAPI'
 export default function SearchBooks() {
+
+    const [query, setQuery] = useState('');
+    const [searchedBooks, setSearchedBooks] = useState([]);
+
+    const updateQueryHandler = (searchQuery) => {
+      setQuery(searchQuery.trim())
+        CallingAPI.search(query, 20).then((res) => {
+          if(Object.prototype.toString.call(res) === '[object Array]')
+          {
+            setSearchedBooks(res)
+          }
+          else{
+            setSearchedBooks([])
+          }
+        })
+      
+    }
+    const clearQueryHandler = () => {
+        updateQueryHandler('')
+    }
+
     return (
         <div className="search-books">
             <div className="search-books-bar">
-                <Link to="/" className="close-search">Close</Link>
+                <Link to="/" className="close-search" onClick={() => {clearQueryHandler()}}>Close</Link>
               <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search..."/>
+                <input type="text" placeholder="Search..." onEmptied={() => {clearQueryHandler()}} onChange={(e) => {updateQueryHandler(e.target.value)}} value={query}/>
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+              {query !== '' && searchedBooks.map((book) => (
+                <li key={book.id}>
+                    <Book />
+                    {book.title}
+                </li>
+              ))}
+              </ol>
             </div>
           </div>
     )
